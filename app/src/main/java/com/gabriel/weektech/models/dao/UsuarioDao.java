8bad63f6ceef8609f4229dd53e8712c4a01e062f
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-04-30 21:50:03 -0300

    Criando a classe de acesso aos objetos

---
+++ app/src/main/java/com/gabriel/weektech/models/dao/UsuarioDao.java
@@ -0,0 +1,19 @@
+package com.gabriel.weektech.models.dao;
+
+
+import androidx.room.*;
+
+import com.gabriel.weektech.models.Usuario;
+
+@Dao
+public interface UsuarioDao {
+
+    @Insert
+    void inserir(Usuario usuario);
+
+    @Query("SELECT * FROM usuario WHERE email = :email AND senha = :senha")
+    Usuario login(String email, String senha);
+
+    @Query("SELECT * FROM usuario")
+    List<Usuario> listar();
+}
