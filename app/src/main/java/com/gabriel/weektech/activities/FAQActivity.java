f518bd7b3d2e116406ee430d4a634bfdd6100e13
GabrielVancoDev <gabrielvanco111223@gmail.com>
2026-05-18 20:02:08 -0300

    Ajustando o layout

--- app/src/main/java/com/gabriel/weektech/activities/FAQActivity.java
+++ app/src/main/java/com/gabriel/weektech/activities/FAQActivity.java
@@ -1,8 +1,23 @@
 package com.gabriel.weektech.activities;
 
+import android.os.Bundle;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.gabriel.weektech.R;
 
 public class FAQActivity extends AppCompatActivity {
 
+    ImageButton btnVoltar;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(v -> finish());
+    }
 }
